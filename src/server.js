f2f93d5787933bcb43a531938c9abc3a654c336c
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2025-06-04 02:33:37 +0100

    port added

--- src/server.js
+++ src/server.js
@@ -61,9 +61,10 @@ app.use((req, res, next) => {
   res.status(404).json({ error: 'Not Found' });
 });
 
+
 // console.log(listEndpoints(app));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
